26dae931b9c82ca28965791fffac6c0011cd1e16
bentleyminwa <bentleyminwa@gmail.com>
2025-01-09 09:09:33 +0300

    refactor authentication pages to use FormComponent and context for state management

--- src/Features/Auth/Components/FormComponent.tsx
+++ src/Features/Auth/Components/FormComponent.tsx
@@ -1,5 +1,41 @@
+import { useContext } from "react";
+import { AuthContext } from "../Context/AuthStore";
+
 const FormComponent = () => {
-  return <form action=""></form>;
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+
+  const { formData, error, handleChange } = context;
+
+  return (
+    <>
+      <div className="mb-4">
+        <label className="block mb-2 font-semibold">Email</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+          className="w-full p-2 border rounded-md"
+        />
+      </div>
+      <div className="mb-4">
+        <label className="block mb-2 font-semibold">Password</label>
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+          className="w-full p-2 border rounded-md"
+        />
+      </div>
+      {error && <p className="text-red-500 mb-4">{error}</p>}
+    </>
+  );
 };
 
 export default FormComponent;
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
-import { AuthProvider } from "./Features/Auth/Context/AuthStore.ts";
+import { AuthProvider } from "./Features/Auth/Context/AuthStore.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
