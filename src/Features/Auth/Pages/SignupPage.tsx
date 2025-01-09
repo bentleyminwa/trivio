debe8829fea18eb9e7b63cf0e8f1bd5c97208169
bentleyminwa <bentleyminwa@gmail.com>
2025-01-09 10:08:41 +0300

    add LabelComponent and update SignupPage layout for improved structure and styling

--- src/Features/Auth/Pages/SignupPage.tsx
+++ src/Features/Auth/Pages/SignupPage.tsx
@@ -7,28 +7,39 @@ const SignupPage = () => {
   const { handleSignup, handleChange, formData } = useAuth();
 
   return (
-    <div className="flex flex-col items-center p-6">
-      <h1 className="text-2xl font-bold mb-4">Signup</h1>
-      <form
-        onSubmit={handleSignup}
-        className="w-full max-w-sm p-6 bg-white shadow-md rounded-lg"
-      >
-        <div className="mb-4">
-          <label className="block mb-2 font-semibold">Name</label>
-          <Input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-          />
-        </div>
-        <FormComponent />
-        <Button>Create Account</Button>
-      </form>
-      <p>
-        Already have an account? <a href="/login">login</a>
-      </p>
-    </div>
+    <main className="h-screen">
+      <header>
+        <h1 className="py-10 text-4xl text-accent font-bold text-center">
+          Welcome to Trivio
+        </h1>
+      </header>
+      <section className="flex flex-col items-center">
+        <h2 className="text-xl font-bold mt-7 text-gray-600">
+          Register for an Account
+        </h2>
+        <form
+          onSubmit={handleSignup}
+          className="w-full max-w-sm my-7 p-6 bg-white shadow-md rounded-lg"
+        >
+          <div className="mb-4">
+            <label className="block mb-2 font-semibold text-gray-500">
+              Name
+            </label>
+            <Input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </div>
+          <FormComponent />
+          <Button>Create Account</Button>
+        </form>
+        <p>
+          Already have an account? <a href="/login">login</a>
+        </p>
+      </section>
+    </main>
   );
 };
 
